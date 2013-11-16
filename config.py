d81ac34ca2de4fd5335d93e22667897ccc96ad91
Ivan De Marino <detronizator@gmail.com>
2013-11-16 23:41:42 +0000

    Keeping configuration generic for 'master'

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 import logging
 
 # Default URL: if no Redirect URL can be generated, traffic will be diverted here
-DEFAULT_URL = 'http://ivandemarino.me/'
+DEFAULT_URL = 'http://default.new-domain.com/'
 
 # Mapping of Source URLs to Target URLs.
 # You can choose to map in 2 ways:
@@ -11,17 +11,15 @@ DEFAULT_URL = 'http://ivandemarino.me/'
 # Format:
 #    URLS = { 'Old Domain': ('New Domain', 'Discard URL Path part and Redirect to Root?'), ... }
 URLS = {
-   '%s.appspot.com' % (os.environ['APPLICATION_ID']) : ('ivandemarino.me', False),
-   'redirector.ivandemarino.me' : ('ivandemarino.me', False),
-   'detronizator.org': ('ivandemarino.me', False),
-   'www.detronizator.org': ('ivandemarino.me', False),
-   'blog.ivandemarino.me': ('ivandemarino.me', False),
-   'downloads.detronizator.org': ('ivandemarino.me', True),
+   '%s.appspot.com' % (os.environ['APPLICATION_ID']) : ('www.new-domain.com', False),
+   'www.old-domain.com' : ('www.new-domain.com', False),
+   'downloads.old-domain.com': ('downloads.new-domain.com', False),
+   'stuff.old-domain.com': ('stuff.new-domain.com', True),
 };
 
 # Send email message when redirect error occurs
 ERROR_EMAIL_ACTIVE = True;
-ERROR_EMAIL_SENDER = '"Redirector (ivandemarino.me)" <detronizator@gmail.com>';
+ERROR_EMAIL_SENDER = '"Redirector for new-domain.com" <your@email.com>';
 ERROR_EMAIL_SUBJECT = 'Redirect Script Error';
 ERROR_EMAIL_BODY = 'Unable to redirect this url: ';
 
